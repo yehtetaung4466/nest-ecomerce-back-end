21d02da4ddf0ef129832024ddc7c4effa3361051
yehtetaung <yeaung1234@yahoo.com>
2023-12-11 14:41:21 +0630

    done md

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,10 +6,6 @@ export default {
   out: './drizzle',
   driver: 'pg',
   dbCredentials: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
-  },
+    connectionString: process.env.DB_URL,
+  }
 } satisfies Config;
--- src/drizzle/drizzle.service.ts
+++ src/drizzle/drizzle.service.ts
@@ -7,14 +7,7 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class DrizzleService {
   constructor(private configService: ConfigService) {}
-  private readonly connection = postgres({
-    host: this.configService.get('DB_HOST'),
-    user: this.configService.get('DB_USER'),
-    password: this.configService.get('DB_PASSWORD'),
-    port: this.configService.get('DB_PORT'),
-    database: this.configService.get('DB_NAME'),
-  });
-  
+  private readonly connection = postgres(this.configService.get('DB_URL'));
   public db = drizzle(this.connection, {
     schema,
   });
